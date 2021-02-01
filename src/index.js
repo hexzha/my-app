2489e4392fdd533f6bd490e396fb448ad07fec08
Hexiang Zhang <sam10.zhang@gmail.com>
2021-01-31 18:16:12 -0600

    use states

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null;
+      value: null,
     }
   }
   
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
